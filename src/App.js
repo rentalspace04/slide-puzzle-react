e2549832b22171293544d1efe03a864eef4419b4
Danyon Ramsay <danyon.ramsay@yahoo.com.au>
2018-05-05 13:05:55 +1000

    Implemented shuffling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import TileBoard from "./TileBoard"
 import Dimensions from './Dimensions'
 import _ from "lodash"
 
+const randomInt = (min, max) => {
+    return Math.floor((max - min) * Math.random()) + min
+}
+
 class App extends Component {
     constructor(props) {
         super(props)
@@ -16,6 +20,8 @@ class App extends Component {
         this.movesInto = this.movesInto.bind(this)
         this.moveTile = this.moveTile.bind(this)
         this.outsideBoard = this.outsideBoard.bind(this)
+        this.shuffleTiles = this.shuffleTiles.bind(this)
+        this.makeRandomMove = this.makeRandomMove.bind(this)
     }
 
     changeDimensions(newDim, isN) {
@@ -40,12 +46,49 @@ class App extends Component {
             const start = 1 + cols * i
             tiles[i] = _.range(start, start + cols)
         }
-        tiles[rows - 1][cols- 1] = 0
+        tiles[rows - 1][cols - 1] = 0
         return tiles;
     }
 
     shuffleTiles() {
+        let i = 0;
+        const shuffles = 10 * this.state.m * this.state.n;
+        const sleepMove = () => {
+            this.makeRandomMove()
+            if (i < shuffles) {
+                i++;
+                setTimeout(sleepMove, 50)
+            }
+        }
+        setTimeout(sleepMove, 10)
+        
+    }
 
+    makeRandomMove() {
+        // Possible moves that could be made
+        console.log("Making ranom move")
+        const moves = [[1, 0], [-1, 0], [0, 1], [0, -1]];
+        console.log(this.state.tiles)
+        const zero_j = _.findIndex(this.state.tiles, row => _.includes(row, 0))
+        const zero_i = _.findIndex(this.state.tiles[zero_j], elem => elem === 0)
+        // Find a valid move (starting from random)
+        let i = randomInt(0, 4);
+        let checked = 0;
+        while (checked < 4) { // Should never have to rely on this, but JIC
+            const move = moves[i];
+            const move_i = zero_i + move[0];
+            const move_j = zero_j + move[1];
+            console.log(`moving ${move_i}, ${move_j}`)
+            // If this is a valid move, do it
+            if (!this.outsideBoard(move_i, move_j)) {
+                console.log("inside board")
+                this.moveTile(move_i, move_j)
+                return;
+            }
+            // Otherwise, check next move in order
+            i = (i + 1) % 4;
+            checked++;
+        }
     }
 
     moveTile(i, j) {
--- src/Tile.js
+++ src/Tile.js
@@ -5,9 +5,13 @@ class TileRow extends Component {
     render() {
         const i = this.props.colNum
         const j = this.props.rowNum
+        let elem = "" + this.props.element;
+        if (elem === 0) {
+            elem = "."; // For some reason a space/empty string gives formating troubles
+        }
         return (
             <div className="Tile" onClick={(e) => this.props.move(i, j)}>
-                {this.props.element}
+                {elem}
             </div>
         );
     }
